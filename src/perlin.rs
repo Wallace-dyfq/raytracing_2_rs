41b617cb89aa28ebd9656287de337fbb0458aebd
wallace <wallace.dyfq@gmail.com>
2023-09-07 17:53:55 -0600

    perlin texture with trilinear interpolation

--- src/perlin.rs
+++ src/perlin.rs
@@ -31,14 +31,43 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &Point3) -> f64 {
-        let i = (4.0 * p.x()) as i32 & 255;
-        let j = (4.0 * p.y()) as i32 & 255;
-        let k = (4.0 * p.z()) as i32 & 255;
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
 
-        self.ranfloat
-            [(self.perm_x[i as usize] ^ self.perm_y[j as usize] ^ self.perm_z[k as usize]) as usize]
+        let i = (p.x().floor()) as i32;
+        let j = (p.y().floor()) as i32;
+        let k = (p.z().floor()) as i32;
+        let mut c = vec![vec![vec![0.0; 2]; 2]; 2];
+
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    let ii = ((i + di as i32) & 255) as usize;
+                    let jj = ((j + dj as i32) & 255) as usize;
+                    let kk = ((k + dk as i32) & 255) as usize;
+                    c[di][dj][dk] = self.ranfloat
+                        [(self.perm_x[ii] ^ self.perm_y[jj] ^ self.perm_z[kk]) as usize]
+                }
+            }
+        }
+        Self::trilinear_interp(c, u, v, w)
     }
 
+    fn trilinear_interp(c: Vec<Vec<Vec<f64>>>, u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    accum += (i as f64 * u + (1 - i) as f64 * (1.0 - u))
+                        * (j as f64 * v + (1 - j) as f64 * (1.0 - v))
+                        * (k as f64 * w + (1 - k) as f64 * (1.0 - w))
+                        * c[i][j][k];
+                }
+            }
+        }
+        accum
+    }
     fn perlin_generate_perm() -> Vec<i32> {
         let mut p = vec![0; POINT_COUNT];
         for i in 0..POINT_COUNT {
